18c80d2a453907fe50ab93cefae8fdea597bc967
ViavaCos <13811092694@163.com>
2021-10-25 11:01:31 +0800

    feat: 导出生命周期

--- src/main.js
+++ src/main.js
@@ -4,7 +4,32 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+let instance;
+
+const render = function(container){
+  instance = new Vue({
+    router,
+    render: h => h(App)
+  }).$mount(container)
+}
+
+// 子应用独立运行
+!window.__POWERED_BY_QIANKUN__ && render('#app')
+
+/**
+ * 子应用导出三个必要生命周期:
+ * bootstrap、mount、unmount
+ */
+export async function bootstrap(){
+  console.log('bootstrap..');
+}
+
+export async function mount(props = {}){
+  const { container } = props
+  render(container.querySelector('#app') || '#app')
+  console.log(instance);
+}
+
+export async function unmount(){
+  instance.destroy()
+}
